3c5850047bcc6043075336575d027a4345d7460d
Jambrose <ambrose_john@yahoo.com>
2021-04-25 19:09:18 -0500

    Uncomment turning off port 3000 alternative way

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-//commented out to see if it would close the firewall
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+	res.send('Hello World! from express!');
+});
 
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-// 	res.send('Hello World! from express!');
-// });
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
