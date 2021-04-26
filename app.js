6e8ef627d275b29bdfe20b85f24f9bcb495637d5
Jambrose <ambrose_john@yahoo.com>
2021-04-25 19:03:58 -0500

    Comment to close port 3000

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+//commented out to see if it would close the firewall
 
-app.get('/', (req, res) => {
-	res.send('Hello World! from express!');
-});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+// 	res.send('Hello World! from express!');
+// });
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
